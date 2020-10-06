da8d5e3d87b5269a131bce7f5fd1d85129e1056b
tienzlatan <39383420+tienzlatan@users.noreply.github.com>
2020-10-06 23:28:00 +0700

    Updates

---
+++ precache-manifest.1a76e36055e6b7f13901fd0867373c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b36a60448c63e95e3049ff1d4039df4",
+    "url": "/reactjs-tutorial-tictactoe/index.html"
+  },
+  {
+    "revision": "371e0c5be945dcff1330",
+    "url": "/reactjs-tutorial-tictactoe/static/css/main.d836b613.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/reactjs-tutorial-tictactoe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-tutorial-tictactoe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371e0c5be945dcff1330",
+    "url": "/reactjs-tutorial-tictactoe/static/js/main.5980d3c0.chunk.js"
+  },
+  {
+    "revision": "79d10036f1a4c9f32f4d",
+    "url": "/reactjs-tutorial-tictactoe/static/js/runtime-main.f18fdbfc.js"
+  }
+]);
